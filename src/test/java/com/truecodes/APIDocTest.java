28e6dcd72522cdbb3b621e9fb3e44ddaad3424ff
divyanshu singh <divyanshus@testvagrant.com>
2024-11-07 12:29:30 +0530

    Test 1 passed With rest assured completed for all 5 APIs

--- src/test/java/com/truecodes/APIDocTest.java
+++ src/test/java/com/truecodes/APIDocTest.java
@@ -5,17 +5,11 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,7 +23,6 @@ public class APIDocTest extends BaseSetup{
     Map<String, String> api9=new HashMap<>();
     Map<String, String> api13=new HashMap<>();
 
-    public static final String PAGE_URL = "https://automationexercise.com/";
 
 
     public void testProductSchemaValidation(String apiUrl) {
@@ -52,18 +45,8 @@ public class APIDocTest extends BaseSetup{
         System.out.println("Response matches the expected schema.");
     }
 
-    @Test
-    public void testApiLists() throws InterruptedException {
-        // Navigate to APIs list for practice
-        driver.findElement(By.xpath("//div[@class='item active']//button[@type='button'][normalize-space()='APIs list for practice']")).click();
-        // expanding 5 apis
-        expandApi1();
-        expandApi2();
-        expandApi4();
-        expandApi9();
-        expandApi14();
-
-        // Make API Request: save response for all 5 apis
+    @Test(priority = 2)
+    public void validateApi1(){
         // API1 GET Response
         // Send the request using RestAssured
         Response responseApi1 = RestAssured.given()
@@ -95,6 +78,137 @@ public class APIDocTest extends BaseSetup{
         System.out.println("Total number of products: " + productCount);
         //Validate schema
         testProductSchemaValidation(api1.get("url"));
+    }
+
+    @Test(priority = 3)
+    public void validateApi2(){
+        RestAssured.baseURI = api2.get("url");
+
+        // Send the POST request
+        Response response = RestAssured
+                .given()
+                .contentType("application/json")
+                .post();
+
+        // Get the response body as a string
+        String responseBody = response.asString();
+
+        // Print the response body (which will be in HTML format)
+        System.out.println("Response Body: " + responseBody);
+
+        // Check if the status code is 405
+        Assert.assertEquals(response.jsonPath().getInt("responseCode"), Integer.parseInt(api2.get("statusCode")), "Expected status code is 200");
+        // Verify response message
+        Assert.assertEquals(response.jsonPath().getString("message"), api2.get("response").trim(), "Response message does not match the expected message");
+    }
+
+    @Test(priority = 4)
+    public void validateApi4(){
+        RestAssured.baseURI = api4.get("url");
+
+        // Send the POST request
+        Response response = RestAssured
+                .given()
+                .contentType("application/json")
+                .put();
+
+        // Get the response body as a string
+        String responseBody = response.asString();
+
+        // Print the response body (which will be in HTML format)
+        System.out.println("Response Body: " + responseBody);
+
+        // Check if the status code is 405
+        Assert.assertEquals(response.jsonPath().getInt("responseCode"), Integer.parseInt(api4.get("statusCode")), "Expected status code is 405");
+        // Verify response message
+        Assert.assertEquals(response.jsonPath().getString("message"), api4.get("response").trim(), "Response message does not match the expected message");
+    }
+    @Test(priority = 5)
+    public void validateApi9(){
+        RestAssured.baseURI = api9.get("url");
+
+        // Send the POST request
+        Response response = RestAssured
+                .given()
+                .contentType("application/json")
+                .delete();
+
+        // Get the response body as a string
+        String responseBody = response.asString();
+
+        // Print the response body (which will be in HTML format)
+        System.out.println("Response Body: " + responseBody);
+
+        // Check if the status code is 405
+        Assert.assertEquals(response.jsonPath().getInt("responseCode"), Integer.parseInt(api9.get("statusCode")), "Expected status code is 405");
+        // Verify response message
+        Assert.assertEquals(response.jsonPath().getString("message"), api9.get("response").trim(), "Response message does not match the expected message");
+    }
+
+    @Test(priority =6)
+    public void validateApi14(){
+        RestAssured.baseURI = api13.get("url");
+        RestAssured.baseURI = "https://automationexercise.com/api/getUserDetailByEmail";
+
+        String email = "example@example.com";
+
+        Response response = RestAssured.given()
+                .queryParam("email", email)
+                .get();
+
+        String responseBody = response.asString();
+
+        System.out.println("Response Body: " + responseBody);
+
+        // Assert the response code is 200 OK
+        Assert.assertEquals(response.jsonPath().getInt("responseCode"), Integer.parseInt(api13.get("statusCode")), "Expected status code is 200");
+        // Assert response contatins user detail
+
+        JsonPath jsonResponse = response.jsonPath();
+
+        // Extract user details from the response
+        int userId = jsonResponse.getInt("user.id");
+        String userName = jsonResponse.getString("user.name");
+        String userEmail = jsonResponse.getString("user.email");
+        String userTitle = jsonResponse.getString("user.title");
+        String userBirthDay = jsonResponse.getString("user.birth_day");
+        String userBirthMonth = jsonResponse.getString("user.birth_month");
+        String userBirthYear = jsonResponse.getString("user.birth_year");
+        String userFirstName = jsonResponse.getString("user.first_name");
+        String userLastName = jsonResponse.getString("user.last_name");
+        String userAddress1 = jsonResponse.getString("user.address1");
+        String userCountry = jsonResponse.getString("user.country");
+        String userState = jsonResponse.getString("user.state");
+        String userCity = jsonResponse.getString("user.city");
+        String userZipcode = jsonResponse.getString("user.zipcode");
+
+        // Perform assertions on the extracted user details
+        Assert.assertEquals(userId, 12771, "User ID is incorrect");
+        Assert.assertEquals(userName, "Test", "User name is incorrect");
+        Assert.assertEquals(userEmail, email, "User email does not match the expected email");
+        Assert.assertEquals(userTitle, "Mrs", "User title is incorrect");
+        Assert.assertEquals(userBirthDay, "12", "User birth day is incorrect");
+        Assert.assertEquals(userBirthMonth, "4", "User birth month is incorrect");
+        Assert.assertEquals(userBirthYear, "2002", "User birth year is incorrect");
+        Assert.assertEquals(userFirstName, "Test", "User first name is incorrect");
+        Assert.assertEquals(userLastName, "Test", "User last name is incorrect");
+        Assert.assertEquals(userAddress1, "First line of address", "User address is incorrect");
+        Assert.assertEquals(userCountry, "United States", "User country is incorrect");
+        Assert.assertEquals(userState, "Alabama", "User state is incorrect");
+        Assert.assertEquals(userCity, "Alabama", "User city is incorrect");
+        Assert.assertEquals(userZipcode, "ABA-JASW", "User zipcode is incorrect");
+    }
+
+    @Test(priority = 1)
+    public void testApiLists() throws InterruptedException {
+        // Navigate to APIs list for practice
+        driver.findElement(By.xpath("//div[@class='item active']//button[@type='button'][normalize-space()='APIs list for practice']")).click();
+        // expanding 5 apis
+        expandApi1();
+        expandApi2();
+        expandApi4();
+        expandApi9();
+        expandApi14();
 
     }
     // expanding API1
@@ -162,7 +276,7 @@ public class APIDocTest extends BaseSetup{
 
         // Extract the Response JSON
         WebElement responseJsonElement = driver.findElement(By.xpath("/html[1]/body[1]/section[1]/div[1]/div[3]/div[1]/div[2]/ul[1]/li[4]"));
-        String responseJson = responseJsonElement.getText().replace("Response Message ", "");
+        String responseJson = responseJsonElement.getText().replace("Response Message: ", "");
         api2.put("url",apiUrl);
         api2.put("method",requestMethod);
         api2.put("statusCode",responseCode);
